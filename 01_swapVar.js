5e005823bccad3971a835211d536eb76dc9e7b0f
Lisa-467 <shelavanedipti@gmail.com>
2023-12-26 21:26:55 +0530

    Var declaration and intialization
    Var declaration and intialization

---
+++ 01_swapVar.js
@@ -0,0 +1,11 @@
+A = "10";
+B = "20";
+console.log("Before swap :");
+console.log("A = ", A);
+console.log("B = ", B);
+var temp = A;
+A = B;
+B = temp;
+console.log("After  swap :");
+console.log("A = ", A);
+console.log("B = ", B);
---
+++ hello.js
@@ -0,0 +1,13 @@
+console.log("Hello JS , I am Dipti");
+
+var myName; // Varibale Declartion
+console.log(myName); // undefined
+
+myName = "Dipti"; // Variable Intialization
+console.log("First Name: ", myName);
+
+var age = "32"; // Varibale declaration and Initialization
+console.log("Age:", age);
+
+myName = "Dipti Shelavane"; // Varibale update
+console.log("Full name: ", myName);
